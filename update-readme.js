4a672837938502705bd4cf79e9289ce464a1168a
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-07-01 12:06:16 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -3,15 +3,12 @@ const fs = require('fs');
 const data = fs.readFileSync('data.json');
 const animeData = JSON.parse(data);
 
-let readmeContent = `<h1 align="center">Daftar Anime Terbaru</h1>\n\n`;
+let readmeContent = `# Daftar Anime Terbaru\n\n`;
+readmeContent += `| Judul | Gambar | Episode | Hari | Tanggal | Link |\n`;
+readmeContent += `|-------|--------|---------|------|---------|------|\n`;
 
 animeData.result.forEach((anime) => {
-  readmeContent += `<h2 align="center">${anime.title}\n</h2>`;
-  readmeContent += `<img align="center" src="${anime.thumb}">\n\n`;
-  readmeContent += `<h3 align="center">Episode :</h3> ${anime.eps}\n`;
-  readmeContent += `<h3 align="center">Hari :</h3> ${anime.day}\n`;
-  readmeContent += `<h3 align="center">Tanggal :</h3> ${anime.date}\n`;
-  readmeContent += `[Link](${anime.link})\n\n`;
+  readmeContent += `| ${anime.title} | ![${anime.title}](${anime.thumb}) | ${anime.eps} | ${anime.day} | ${anime.date} | [Link](${anime.link}) |\n`;
 });
 
 fs.writeFileSync('README.md', readmeContent);
