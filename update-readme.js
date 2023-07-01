0a867de28ffebf246f321a67a6cbb09e64308f6b
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-07-01 12:30:49 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -3,9 +3,44 @@ const fs = require('fs');
 const data = fs.readFileSync('data.json');
 const animeData = JSON.parse(data);
 
-let readmeContent = `# Daftar Anime Terbaru\n\n`;
+let readmeContent = `<h1>Daftar Anime Terbaru</h1>\n\n`;
 
 animeData.result.forEach((anime) => {
+  readmeContent += `<table align="center">`;
+  readmeContent += `<tr>`;
+  readmeContent += `<th><h3 align="center">${anime.title}</h3></th>`;
+  readmeContent += `</tr>`;
+  readmeContent += `<tr>`;
+  readmeContent += `<td>`;
+  readmeContent += `<p>`;
+  readmeContent += `<img src="${anime.thumb}", height="256">`;
+  readmeContent += `</p>`;
+  readmeContent += `</td>`;
+  readmeContent += `</tr>`;
+  readmeContent += `<tr>`;
+  readmeContent += `<td>`;
+  readmeContent += `<table>`;
+  readmeContent += `<tr>`;
+  readmeContent += `<td>Episode :</td>`;
+  readmeContent += `<td align="center">${anime.eps}</td>`;
+  readmeContent += `</tr>`;
+  readmeContent += `<tr>`;
+  readmeContent += `<td>Hari :</td>`;
+  readmeContent += `<td align="center">${anime.day}</td>`;
+  readmeContent += `</tr>`;
+  readmeContent += `<tr>`;
+  readmeContent += `<td>Tanggal :</td>`;
+  readmeContent += `<td align="center">${anime.date}</td>`;
+  readmeContent += `</tr>`;
+  readmeContent += `<tr>`;
+  readmeContent += `<td>Link :</td>`;
+  readmeContent += `<td align="center">[Watching the Anime](${anime.link})</td>`;
+  readmeContent += `</tr>`;
+  readmeContent += `</table>`;
+  readmeContent += `</td>`;
+  readmeContent += `</tr>`;
+  readmeContent += `</table>`;
+  
   readmeContent += `## ${anime.title}\n`;
   readmeContent += `![${anime.title}](${anime.thumb})\n\n`;
   readmeContent += `**Episode**: ${anime.eps}\n\n`;
