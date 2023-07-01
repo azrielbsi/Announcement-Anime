ba2cee5ea50c067f040205d45a2c1574cef9d93c
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-07-01 11:19:30 +0700

    Add files via upload

---
+++ update-readme.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('data.json');
+const animeData = JSON.parse(data);
+
+let readmeContent = `# Daftar Anime Terbaru\n\n`;
+
+animeData.result.forEach((anime) => {
+  readmeContent += `## ${anime.title}\n`;
+  readmeContent += `![${anime.title}](${anime.thumb})\n\n`;
+  readmeContent += `**Episode**: ${anime.eps}\n`;
+  readmeContent += `**Hari**: ${anime.day}\n`;
+  readmeContent += `**Tanggal**: ${anime.date}\n`;
+  readmeContent += `[Link](${anime.link})\n\n`;
+});
+
+fs.writeFileSync('README.md', readmeContent);
