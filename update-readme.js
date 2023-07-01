6272ef051de69a99b392d6d8221586b356b7b345
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-07-01 12:34:24 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const data = fs.readFileSync('data.json');
 const animeData = JSON.parse(data);
 
-let readmeContent = `<h1>Daftar Anime Terbaru</h1>\n\n`;
+let readmeContent = `<h1 align="center">Daftar Anime Terbaru</h1>\n\n`;
 
 animeData.result.forEach((anime) => {
   readmeContent += `<table align="center">`;
@@ -12,7 +12,7 @@ animeData.result.forEach((anime) => {
   readmeContent += `</tr>`;
   readmeContent += `<tr>`;
   readmeContent += `<td>`;
-  readmeContent += `<p>`;
+  readmeContent += `<p align="center">`;
   readmeContent += `<img src="${anime.thumb}", height="256">`;
   readmeContent += `</p>`;
   readmeContent += `</td>`;
@@ -34,19 +34,12 @@ animeData.result.forEach((anime) => {
   readmeContent += `</tr>`;
   readmeContent += `<tr>`;
   readmeContent += `<td>Link :</td>`;
-  readmeContent += `<td align="center">[Watching the Anime](${anime.link})</td>`;
+  readmeContent += `<td align="center">${anime.link}</td>`;
   readmeContent += `</tr>`;
   readmeContent += `</table>`;
   readmeContent += `</td>`;
   readmeContent += `</tr>`;
   readmeContent += `</table>`;
-  
-  readmeContent += `## ${anime.title}\n`;
-  readmeContent += `![${anime.title}](${anime.thumb})\n\n`;
-  readmeContent += `**Episode**: ${anime.eps}\n\n`;
-  readmeContent += `**Hari**: ${anime.day}\n\n`;
-  readmeContent += `**Tanggal**: ${anime.date}\n\n`;
-  readmeContent += `[Link](${anime.link})\n\n`;
 });
 
 fs.writeFileSync('README.md', readmeContent);
