ae9bf237378814563149f621d86166f354cecb3a
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-07-17 23:45:17 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -8,12 +8,12 @@ let readmeContent = `<h1 align="center">Daftar Anime Terbaru</h1>\n\n`;
 animeData.result.forEach((anime) => {
   readmeContent += `<table align="center">`;
   readmeContent += `<tr>`;
-  readmeContent += `<th><h3 align="center">${anime.title}</h3></th>`;
+  readmeContent += `<th><h3 align="center">${anime.judul}</h3></th>`;
   readmeContent += `</tr>`;
   readmeContent += `<tr>`;
   readmeContent += `<td>`;
   readmeContent += `<p align="center">`;
-  readmeContent += `<img src="${anime.thumb}", height="256">`;
+  readmeContent += `<img src="${anime.thumbnail}", height="256">`;
   readmeContent += `</p>`;
   readmeContent += `</td>`;
   readmeContent += `</tr>`;
@@ -34,7 +34,7 @@ animeData.result.forEach((anime) => {
   readmeContent += `</tr>`;
   readmeContent += `<tr>`;
   readmeContent += `<td>Link :</td>`;
-  readmeContent += `<td align="center"><a href="${anime.link}">Waching the Anime</a></td>`;
+  readmeContent += `<td align="center"><a href="${anime.link}">Anime Information</a></td>`;
   readmeContent += `</tr>`;
   readmeContent += `</table>`;
   readmeContent += `</td>`;
