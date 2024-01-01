4d2a6d6b2d3bfc758f59537709a9c07db49ce002
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-01-02 03:49:43 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,45 +1,46 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('data.json');
+const data = fs.readFileSync('data.json', 'utf8'); // Tambahkan 'utf8' untuk membaca file sebagai string
 const animeData = JSON.parse(data);
 
-let readmeContent = `<h1 align="center">Daftar Anime Terbaru</h1>`;
+let readmeContent = `<h1 align="center">Daftar Anime Terbaru</h1>\n\n`; // Tambahkan baris kosong setelah judul
 
 // Fungsi untuk menulis hasil ke file README.md
 animeData.result.forEach((anime) => {
-   readmeContent += `<table align="center">`;
-   readmeContent += `<tr>`;
-   readmeContent += `<th><h3 align="center">${anime.title}</h3></th>`;
-   readmeContent += `</tr>`;
-   readmeContent += `<tr>`;
-   readmeContent += `<td>`;
-   readmeContent += `<p align="center">`;
-   readmeContent += `<img src="${anime.thumb}", height="256">`;
-   readmeContent += `</p>`;
-   readmeContent += `</td>`;
-   readmeContent += `</tr>`;
-   readmeContent += `<td>`;
-   readmeContent += `<table align="center">`;
-   readmeContent += `<tr>`;
-   readmeContent += `<td>Episode :</td>`;
-   readmeContent += `<td align="center">${anime.eps}</td>`;
-   readmeContent += `</tr>`;
-   readmeContent += `<tr>`;
-   readmeContent += `<td>Tanggal :</td>`;
-   readmeContent += `<td align="center">${anime.date}</td>`;
-   readmeContent += `</tr>`;
-   readmeContent += `<tr>`;
-   readmeContent += `<td>Hari :</td>`;
-   readmeContent += `<td align="center">${anime.day}</td>`;
-   readmeContent += `</tr>`;
-   readmeContent += `<tr>`;
-   readmeContent += `<td>Link :</td>`;
-   readmeContent += `<td align="center"><a href="${anime.link}">Anime Information</a></td>`;
-   readmeContent += `</tr>`;
-   readmeContent += `</table>`;
-   readmeContent += `</td>`;
-   readmeContent += `</tr>`;
-   readmeContent += `</table>`;
-  });
+  readmeContent += `<table align="center">\n`;
+  readmeContent += `<tr>\n`;
+  readmeContent += `<th><h3 align="center">${anime.title}</h3></th>\n`;
+  readmeContent += `</tr>\n`;
+  readmeContent += `<tr>\n`;
+  readmeContent += `<td>\n`;
+  readmeContent += `<p align="center">\n`;
+  readmeContent += `<img src="${anime.thumb}" height="256">\n`; // Hapus tanda koma yang tidak perlu
+  readmeContent += `</p>\n`;
+  readmeContent += `</td>\n`;
+  readmeContent += `</tr>\n`;
+  readmeContent += `<tr>\n`;
+  readmeContent += `<td>\n`;
+  readmeContent += `<table align="center">\n`;
+  readmeContent += `<tr>\n`;
+  readmeContent += `<td>Episode :</td>\n`;
+  readmeContent += `<td align="center">${anime.eps}</td>\n`;
+  readmeContent += `</tr>\n`;
+  readmeContent += `<tr>\n`;
+  readmeContent += `<td>Tanggal :</td>\n`;
+  readmeContent += `<td align="center">${anime.date}</td>\n`;
+  readmeContent += `</tr>\n`;
+  readmeContent += `<tr>\n`;
+  readmeContent += `<td>Hari :</td>\n`;
+  readmeContent += `<td align="center">${anime.day}</td>\n`;
+  readmeContent += `</tr>\n`;
+  readmeContent += `<tr>\n`;
+  readmeContent += `<td>Link :</td>\n`;
+  readmeContent += `<td align="center"><a href="${anime.link}">Anime Information</a></td>\n`;
+  readmeContent += `</tr>\n`;
+  readmeContent += `</table>\n`;
+  readmeContent += `</td>\n`;
+  readmeContent += `</tr>\n`;
+  readmeContent += `</table>\n\n`; // Tambahkan baris kosong setelah setiap entri anime
+});
 
 fs.writeFileSync('README.md', readmeContent);
