53126a2946e3fbf9015b42cade074ba158afdd03
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-01-03 21:01:51 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -26,7 +26,6 @@ async function getLatestAnimeData() {
       link: item.link,
       description: splitDescription(item.contentSnippet),
     }));
-
   } catch (error) {
     console.error('Error fetching feed:', error);
     return [];
@@ -44,29 +43,48 @@ async function updateReadmeWithAnimeData() {
       dateStyle: 'medium',
       timeStyle: 'medium'
     });
-    const maxTables = 20;
-    const tablesToShow = Math.min(animeData.length, maxTables);
 
     let readmeContent = `<p align="center"><a href=""><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&pause=1000&color=FFDA5D&center=true&vCenter=true&repeat=false&width=435&lines=Latest+Anime+List" alt="Typing SVG" /></a></p>\n\n`;
     readmeContent += `<p align="center"><em>Updated on: ${currentDateTime}</em></p>\n\n`;
     readmeContent += `<p align="center"><img src="img/anime-update.jpeg" height="100"></p>`;
     readmeContent += `<p align="center">This script aims to automate the process of updating the latest anime information, so that users do not need to do it manually. This makes it easier for users to know what anime are newly released and makes it easier for them to access more information.</p>`;
 
-    for (let i = 0; i < tablesToShow; i++) {
-      const anime = animeData[i];
-    
-      readmeContent += `
-        <table align="center">
-          <tr><th><h3 align="center">${anime.title}</h3></th></tr>
-          <tr><td><p align="center"><img src="${anime.thumb}" height="256"></p></td></tr>
-          <tr><td><table align="center">
-            <tr><td>📔 Publish Date :</td><td align="center">${anime.date}</td></tr>
-            <tr><td>📕 Link :</td><td align="center"><a href="${anime.link}">Anime Information</a></td></tr>
-            <tr><td colspan="2">📙 Description :</td></tr>
-            <tr><td colspan="2"><p align="center">${anime.description}</p></td></tr>
-          </table></td></tr>
-        </table>\n\n`;
-    }
+    animeData.forEach(anime => {
+      readmeContent += `<table align="center">\n`;
+      readmeContent += `<tr>\n`;
+      readmeContent += `<th><h3 align="center">${anime.title}</h3></th>\n`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `<tr>\n`;
+      readmeContent += `<td>\n`;
+      readmeContent += `<p align="center">\n`;
+      readmeContent += `<img src="${anime.thumb}" height="256">\n`;
+      readmeContent += `</p>\n`;
+      readmeContent += `</td>\n`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `<tr>\n`;
+      readmeContent += `<td>\n`;
+      readmeContent += `<table align="center">\n`;
+      readmeContent += `<tr>\n`;
+      readmeContent += `<td>📔 Publish Date :</td>\n`;
+      readmeContent += `<td align="center">${anime.date}</td>\n`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `<tr>\n`;
+      readmeContent += `<td>📕 Link :</td>\n`;
+      readmeContent += `<td align="center"><a href="${anime.link}">Anime Information</a></td>\n`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `<tr>\n`;
+      readmeContent += `<td colspan="2">📙 Description :</td>`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `<tr>\n`;
+      readmeContent += `<td colspan="2">\n`;
+      readmeContent += `<p align="center">${anime.description}</p>\n`; 
+      readmeContent += `</td>\n`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `</table>\n`;
+      readmeContent += `</td>\n`;
+      readmeContent += `</tr>\n`;
+      readmeContent += `</table>\n\n`;
+    });
 
     fs.writeFileSync('README.md', readmeContent);
     console.log('README.md updated successfully with latest anime data and date!');
