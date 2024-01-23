88fc04610a092f58f7498997552579f9716a683a
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-01-23 19:54:57 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -17,7 +17,7 @@ function splitDescription(description) {
 
 async function getLatestAnimeData() {
   try {
-    const feed = await parser.parseURL('https://feeds.feedburner.com/crunchyroll/rss/anime');
+    const feed = fs.readFileSync('data.json');
     return feed.items.map(item => ({
       title: item.title,
       thumb: item.enclosure.url,
