4f6e76481be2f24905da07bc792bfa6065679cb3
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-01-28 18:20:28 +0700

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -47,11 +47,20 @@ async function updateReadmeWithAnimeData() {
 
     let readmeContent = `<p align="center"><a href=""><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&pause=1000&color=FFDA5D&center=true&vCenter=true&repeat=false&width=435&lines=Latest+Anime+List" alt="Typing SVG" /></a></p>\n\n`;
     readmeContent += `<p align="center"><em>Updated on: ${currentDateTime}</em></p>\n\n`;
-    readmeContent += `<p align="center"><img src="img/anime-update.jpeg" height="100"></p>`;
+    readmeContent += `<p align="center"><img src="img/news.png" height="100"></p>`;
     readmeContent += `<p align="center">This script aims to automate the process of updating the latest anime information, so that users do not need to do it manually. This makes it easier for users to know what anime are newly released and makes it easier for them to access more information.</p>`;
-    readmeContent += `<p align="center"><img align="center" src="https://img.shields.io/github/contributors/${{ github.repository }}"> <img align="center" src="https://img.shields.io/github/issues/${{ github.repository }}"> <img align="center" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=shields"> <img align="center" src="https://img.shields.io/github/issues-pr/${{ github.repository }}"> <a href="https://github.com/${{ github.repository }}/blob/master/LICENSE"><img align="center" src="https://img.shields.io/badge/License-MIT-yellow.svg"></a><img align="center" src="https://img.shields.io/github/commit-activity/m/${{ github.repository }}"> <img align="center" src="https://github.com/Julius-Ulee/github-profile-views-counter/blob/master/svg/738176371/badge.svg"> <img align="center" src="https://github.com/Julius-Ulee/github-profile-views-counter/blob/master/graph/738176371/small/week.png"><br><img align="center" src="https://img.shields.io/maintenance/yes/2024"></p>`;
-    //readmeContent += `${BADGES}`;
-    //readmeContent += `${INFO}`;
+    readmeContent += `<p align="center">
+    <img align="center" src="https://img.shields.io/github/contributors/Julius-Ulee/Announcements-Anime"> 
+    <img align="center" src="https://img.shields.io/github/issues/Julius-Ulee/Announcements-Anime"> 
+    <img align="center" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=shields"> 
+    <img align="center" src="https://img.shields.io/github/issues-pr/Julius-Ulee/Announcements-Anime"> 
+    <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/LICENSE"><img align="center" src="https://img.shields.io/badge/License-MIT-yellow.svg"></a>
+    <img align="center" src="https://img.shields.io/github/commit-activity/m/Julius-Ulee/Announcements-Anime"> 
+    <img align="center" src="https://github.com/Julius-Ulee/github-profile-views-counter/blob/master/svg/738176371/badge.svg"> 
+    <img align="center" height='20' src="https://github.com/Julius-Ulee/github-profile-views-counter/blob/master/graph/738176371/small/week.png"><br>
+    <img align="center" src="https://img.shields.io/maintenance/yes/2024"></p>`;
+    readmeContent += `<p align="center"><b>Display GitHub Action Badge</b> <a href="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/black.yml"><img align="center" src="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/black.yml/badge.svg"></a> <a href="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/jekyll.yml"><img align="center" src="https://github.com/Julius-Ulee/Announcements-Anime/actions/workflows/jekyll.yml/badge.svg"></a></p>`;
+    readmeContent += `<p align="center"><a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/README.md">Readme</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/.github/workflows/black.yml">Actions-YML</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/.gitignore">Gitignore</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/LICENSE">License</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/update-readme.js">Update-readme</a> • <a href="https://github.com/Julius-Ulee/Announcements-Anime/blob/master/package.json">Package</a></p>`;
 
     truncatedAnimeData.forEach(anime => {
       readmeContent += `<table align="center">\n`;
